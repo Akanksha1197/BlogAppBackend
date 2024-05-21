4b5c8984e42e2b7c08a1176d72204a0ae241e278
Akanksha1197 <167187980+Akanksha1197@users.noreply.github.com>
2024-05-21 22:49:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/akki').then(()=>{
 
 })
 
-const allowedOrigins = [
-  'https://blogappfrontend1yeg-jvurfa5bu-akankshas-projects-76b3734f.vercel.app',
-  'https://blogappfrontend1yeg-2ggiau7o2-akankshas-projects-76b3734f.vercel.app'
-];
-
 app.use(cors({
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -31,8 +26,11 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
 }));
+
 
 
 
